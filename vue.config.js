a9cfac346b11f37f961a238198b62348402a5d35
Chetti666 <a.guiachetti@gmail.com>
2025-03-10 18:02:32 -0300

    webpack

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/web/' : '/',
-  outputDir: 'docs'
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/nombre-repo/' : '/',
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 8192, // Tamaño máximo en bytes para convertir a base64
+            name: 'img/[name].[hash:7].[ext]' // Nombre de archivo de salida
+          }
+        }
+      ]
+    }
+  }
+});
